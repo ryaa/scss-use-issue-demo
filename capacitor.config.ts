d5ddd745d0e52635e50e9d472996f48823df75b9
Alex Ryltsov <ryltsov.alex@gmail.com>
2022-11-14 14:45:17 +0200

    initial commit
    This is the project created using ionic cli without any customization

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ryaa.demo',
+  appName: 'ryaa-use-issue-demo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
